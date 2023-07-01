a050ad8b564bba53c751e1718ec7f088538aa08a
JakubKub13 <jakubkubala3@gmail.com>
2023-07-01 12:10:53 +0200

    Finalization of project

--- src/components/OneMovieSlider.js
+++ src/components/OneMovieSlider.js
@@ -1,47 +1,61 @@
 import "./OneMovieSlider.css";
 import data from "../data"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
 const OneMovieSlider = () => {
     const [index, setIndex] = useState(0);
-    const {image, title, description} = data[index];
 
-    const checkMovienumber = (movieIndex) => {
-        if (movieIndex < 0) {
-            return data.length - 1;
-        } else if (movieIndex > data.length - 1) {
-            return 0;
-        } else {
-            return movieIndex;
+    // Filmy v slider sa tocia dokola, ked dojde k zmene indexu, tak sa spusti useEffect
+    useEffect(() => {
+        if (index < 0) {
+            setIndex(data.length - 1);
+        } else if (index > data.length - 1) {
+            setIndex(0);
         }
-    }
+    },[index]);
 
-    const nextMovie = () => {
-        setIndex((index) => {
-            let newIndex = index + 1;
-            return checkMovienumber(newIndex);
-        });
-    };
+    // Automaticke posuvanie slidera
+    useEffect(() => {
+        let setIntervalID = setInterval(() => {
+            setIndex(index + 1);
+        }, 3000)
+        return () => clearInterval(setIntervalID);
+    },[index]);
 
-    const previousMovie = () => {
-        setIndex((index) => {
-            let newIndex = index - 1;
-            return checkMovienumber(newIndex);
-        });
-    };
+    return <section className="all-movies">
+        <div className="all-movies-content">
+            {
+                data.map((oneMovie, oneMovieIndex) => {
+                    const {id, image, title, age, tags, description} = oneMovie;
 
-    return <div className="one-movie">
-        <img src={image} alt="" />
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <button onClick={previousMovie}>
+                    let mainClass = 'next-slide';
+
+                    if (oneMovieIndex === index) {
+                        mainClass = 'active-slide';
+                    }
+
+                    if (oneMovieIndex === index - 1 || (index === 0 && oneMovieIndex === data.length - 1)) {
+                        mainClass = 'last-slide';
+                    }
+
+                    return <article key={id} className={mainClass}>
+                        <img src={image} alt="" />
+                        <h2>{title}</h2>
+                        <p>{description}</p>
+                        <p>{tags}</p>
+                        <p>{age}</p>
+                    </article>
+                })
+            }
+        </div>
+        <button onClick={() => setIndex(index - 1)}>
             <FaArrowAltCircleLeft />
         </button>
-        <button onClick={nextMovie}>
+        <button onClick={() => setIndex(index + 1)}> 
             <FaArrowAltCircleRight />
         </button>
-    </div>
+    </section>
 }
 
 export default OneMovieSlider;
